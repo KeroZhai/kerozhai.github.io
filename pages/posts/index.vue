df8bf9b9c86ba9a89b2a29e0abdca0ef5d3b1887
Kero <980580084@qq.com>
2022-03-24 17:21:51 +0800

    fix: add posts index page

---
+++ pages/posts/index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { useHead } from '@vueuse/head'
+import { formatDate } from '@/utils'
+
+useHead({
+  title: 'Blog - Keroz',
+})
+
+const router = useRouter()
+
+const posts = router.getRoutes()
+  .filter(({ path }) => path.startsWith('/posts') && path !== '/posts')
+  .sort((a, b) => (a.meta.updatedTimestamp - b.meta.updatedTimestamp))
+  .map((route) => {
+    const { title } = route.meta.frontmatter
+
+    return {
+      title: title || route.name,
+      link: route.path,
+      updatedTime: route.meta.updatedTime,
+    }
+  })
+</script>
+
+<template>
+  <div class="prose m-auto">
+    <template v-if="posts.length === 0">
+      Nothing here yet!
+    </template>
+    <template v-else>
+      <app-link
+        v-for="(post, index) in posts"
+        :key="index"
+        :to="post.link"
+        class="item block font-normal mb-6 mt-2 no-underline"
+      >
+        <div class="text-lg">
+          {{ post.title }}
+        </div>
+        <div class="opacity-50 text-sm -mt-1">
+          {{ formatDate(post.updatedTime) }}
+        </div>
+      </app-link>
+    </template>
+  </div>
+</template>
